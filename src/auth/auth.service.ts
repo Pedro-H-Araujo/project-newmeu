56c14d89c0b05566e9781c175e02f60166f87d3b
pedro araujo <pedroha2001@gmail.com>
2025-10-24 10:00:43 -0400

    auth user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UserService) {}
+
+    async signin(params: Prisma.UserCreateInput): Promise<Omit< User, 'password'>> {
+        const user = await this.userService.user({ email: params.email });
+        if (!user)
+            throw new NotFoundException('User not found');
+            const passwordMatch = await bcrypt.compare(params.password, user.password);
+            if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
+
+            const { password, ...result } = user;
+        return result;
+    }
+}
