88d7823e5b34d18f1dc75563afad3a4dc285d127
pedro araujo <pedroha2001@gmail.com>
2025-10-24 10:23:02 -0400

    JWT token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,25 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Inject,Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { Prisma, User } from 'src/prisma/generated/client'; // Corrija o caminho se necessário
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService} from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(private readonly userService: UserService) {}
 
-    async signin(params: Prisma.UserCreateInput): Promise<Omit<User, 'password'>> {
+    @Inject ()
+    private readonly jwtService: JwtService;
+
+    async signin(params: Prisma.UserCreateInput): Promise<{access_token: string}> {
         const user = await this.userService.user({ email: params.email });
         if (!user)
             throw new NotFoundException('User not found');
         const passwordMatch = await bcrypt.compare(params.password, user.password);
         if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
 
-        const { password, ...result } = user;
-        return result;
+        const payload = { sub: user.id };
+        return{access_token: await this.jwtService.signAsync(payload)   };
+
     }
 }
