227912a0a456aa92cc9775dd76c065da9bc4016e
pedro araujo <pedroha2001@gmail.com>
2025-10-24 09:19:05 -0400

    ajustando o banco de dados

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { User as UserModel } from 'src/prisma/generated/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Post()
+    async signupUser(
+        @Body() userData: { email: string; password: string }
+    ): Promise<UserModel> {
+        return this.userService.createUser(userData);
+    }
+}
+     
